e5a0572816f8cc2379c8aa11c9a782b59b0cfbf5
Hussain Juned <hussainjuned99@gmail.com>
2024-08-30 02:13:36 +0100

    some nav links added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import "./App.css";
 
 function App() {
-
   return (
     <>
       <nav>
         <ul>
           <li>
             <a href="#">Home</a>
+            <a href="#">Menu</a>
+            <a href="#">About Us</a>
           </li>
         </ul>
       </nav>
