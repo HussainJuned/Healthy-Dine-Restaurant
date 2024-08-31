75b3b5e3757a7dfaed1b1d2ff1bceedbebbb9b86
Hussain Juned <hussainjuned99@gmail.com>
2024-08-31 01:23:53 +0100

    Project structure added

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-            <a href="#">Menu</a>
-            <a href="#">About Us</a>
-          </li>
-        </ul>
-      </nav>
+
     </>
   );
 }
