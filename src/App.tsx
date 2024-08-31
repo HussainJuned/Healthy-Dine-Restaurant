a18731b766ae2ffb999972f22db39199fe18a544
Hussain Juned <hussainjuned99@gmail.com>
2024-08-31 14:35:02 +0100

    Folder structure added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import HomeIcon from '@mui/icons-material/Home';
 function App() {
   return (
     <>
       <div>
         <h1 className="text-3xl m-5">React TypeScript App</h1>
-        <p className="text-2xl m-5">Start editing to see some magic happen :)</p>
+        <p className="text-2xl m-5">Start editing to see some magic happen :)
+          <HomeIcon/> home
+        </p>
       </div>
     </>
   );
