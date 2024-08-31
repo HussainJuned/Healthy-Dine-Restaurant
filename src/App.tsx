ec05cd49f63183920ca033ac4cc3e9e7ec97defe
Hussain Juned <hussainjuned99@gmail.com>
2024-08-31 01:30:03 +0100

    Tailwind added to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-import "./App.css";
-
 function App() {
   return (
     <>
-
+      <h1 className=" text-indigo-500 text-lg">React TypeScript App</h1>
+      <p>React TypeScript App with Webpack 5</p>
     </>
   );
 }
